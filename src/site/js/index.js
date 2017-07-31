077d1dea67e3172ea71a4d99e7df7621e888292b
Ross Weinstein <ross.a.weinstein@gmail.com>
2017-07-31 15:03:22 -0500

    HRM and postcss cssnext

--- src/site/js/index.js
+++ src/site/js/index.js
@@ -1,4 +1,24 @@
+var styles = require('../css/application.css');
+
 console.log("hi");
 
 var theApp = document.getElementById("app");
 theApp.innerHTML = "It Works";
+
+if (module.hot) {
+    const hotEmitter = require("webpack/hot/emitter");
+    const DEAD_CSS_TIMEOUT = 2000;
+
+    hotEmitter.on("webpackHotUpdate", function(currentHash) {
+        document.querySelectorAll("link[href][rel=stylesheet]").forEach((link) => {
+            const nextStyleHref = link.href.replace(/(\?\d+)?$/, `?${Date.now()}`);
+            const newLink = link.cloneNode();
+            newLink.href = nextStyleHref;
+
+            link.parentNode.appendChild(newLink);
+            setTimeout(() => {
+                link.parentNode.removeChild(link);
+            }, DEAD_CSS_TIMEOUT);
+        });
+    })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,59 @@
-var path = require('path');
-var webpackDev = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require("path");
+var webpackDev = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-var DEVELOPMENT = process.env.NODE_ENV === 'development';
-var PRODUCTION = process.env.NODE_ENV === 'production';
+var DEVELOPMENT = process.env.NODE_ENV === "development";
+var PRODUCTION = process.env.NODE_ENV === "production";
 
 var GENERATE_HTML = new HtmlWebpackPlugin({
-    title: 'Custom Template',
-    minify: {
-        collapseWhitespace: true
-    },
-    template: './src/site/html/template.ejs' 
+  title: "Custom Template",
+  minify: {
+    collapseWhitespace: true
+  },
+  template: "./src/site/html/template.ejs"
 });
 
-var thePugins = PRODUCTION  ? [GENERATE_HTML]
-                            : [ new webpackDev.HotModuleReplacementPlugin(),
-                                GENERATE_HTML];
-
-
-
-module.exports = {
-    entry: './src/site/js/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        compress: true,
-        stats: "errors-only",
-        open: true,
-        openPage: ''
-    },
-    plugins: thePugins
+var DEV_SERVER_CONFIG = {
+  hot: true,
+  contentBase: path.join(__dirname, "dist"),
+  compress: true,
+  stats: "errors-only",
+  open: true,
+  openPage: ""
 };
 
+var POSTCSS = {
+  test: /\.css$/,
+  use: ExtractTextPlugin.extract({
+    use: [
+      {
+        loader: "css-loader",
+        options: { importLoaders: 1 }
+      },
+      "postcss-loader"
+    ]
+  })
+};
+
+var PROJECT_PLUGINS = PRODUCTION
+  ? [GENERATE_HTML, new ExtractTextPlugin("bundle.css")]
+  : [
+      new webpackDev.HotModuleReplacementPlugin(),
+      new webpackDev.NamedModulesPlugin(),
+      GENERATE_HTML,
+      new ExtractTextPlugin("bundle.css")
+    ];
+
+module.exports = {
+  entry: "./src/site/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [POSTCSS]
+  },
+  devServer: DEV_SERVER_CONFIG,
+  plugins: PROJECT_PLUGINS
+};
