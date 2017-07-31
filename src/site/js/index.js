6311267b554d430622b3c0c7f522009bbf73e3de
Ross Weinstein <ross.a.weinstein@gmail.com>
2017-07-31 14:04:39 -0500

    making some progress

--- src/site/js/index.js
+++ src/site/js/index.js
@@ -2,7 +2,3 @@ console.log("hi");
 
 var theApp = document.getElementById("app");
 theApp.innerHTML = "It Works";
-
-if (module.hot) {
-    module.hot.accept();
-}
--- webpack.config.js
+++ webpack.config.js
@@ -5,28 +5,33 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var DEVELOPMENT = process.env.NODE_ENV === 'development';
 var PRODUCTION = process.env.NODE_ENV === 'production';
 
-var entryPoint = PRODUCTION ? [ './src/site/js/index.js' ]
-                            : [ 
-                                './src/site/js/index.js',
-                                'webpack/hot/dev-server',
-                                'webpack-dev-server/client?http://localhost:8080'
-                            ];
+var GENERATE_HTML = new HtmlWebpackPlugin({
+    title: 'Custom Template',
+    minify: {
+        collapseWhitespace: true
+    },
+    template: './src/site/html/template.ejs' 
+});
 
-var thePugins = PRODUCTION  ? []
-                            : [ 
-                                new webpackDev.HotModuleReplacementPlugin(),
-                                new HtmlWebpackPlugin({
-                                    template: './src/site/html/index.ejs',
-                                    inject: "body"
-                                }) 
-                            ];
+var thePugins = PRODUCTION  ? [GENERATE_HTML]
+                            : [ new webpackDev.HotModuleReplacementPlugin(),
+                                GENERATE_HTML];
+
+
 
 module.exports = {
-    entry: entryPoint,
+    entry: './src/site/js/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist',
         filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        stats: "errors-only",
+        open: true,
+        openPage: ''
     },
     plugins: thePugins
 };
+
