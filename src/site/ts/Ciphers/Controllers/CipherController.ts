a1592cb00170e8685e86ba87d69361762b11b239
Ross Weinstein <ross.a.weinstein@gmail.com>
2017-08-01 17:19:15 -0500

    ceasar cipher works

---
+++ src/site/ts/Ciphers/Controllers/CipherController.ts
@@ -0,0 +1,70 @@
+import { CaesarCipherEncoder } from './../model/caesar/CaesarCipherEncoder';
+import { CaesarCipherDecoder } from './../model/caesar/CaesarCipherDecoder';
+import { ICipher } from './../model/ICipher';
+
+export class CipherController {
+
+    public encrypt(text: string, key: string, cipherType: string): string {
+        const type = this.getCipherType(cipherType);
+        return type != CipherType.EMPTY ? this.encryptTheText(text, key, type) : "ERROR: Invalid Cipher Type";
+    }
+
+    private encryptTheText(text: string, key: string, type: CipherType) {
+        const cipher: ICipher = CipherFactory.createCipher(type, CipherMethod.Encrypt);
+        return cipher.performCipher(text, key);
+    }
+
+    public decrypt(text: string, key: string, cipherType: string): string {
+        const type = this.getCipherType(cipherType);
+        return type != CipherType.EMPTY ? this.decryptTheText(text, key, type) : "ERROR: Invalid Cipher Type";
+    }
+
+    private decryptTheText(text: string, key: string, type: CipherType) {
+        const cipher: ICipher = CipherFactory.createCipher(type, CipherMethod.Decrypt);
+        return cipher.performCipher(text, key);
+    }
+
+    private getCipherType(cipher: string): CipherType {
+        if (cipher == "Caesar") {
+            return CipherType.Caesar;
+        } else if (cipher == 'Transposition') {
+            return CipherType.Transposition;
+        } else if (cipher == 'Vigenere') {
+            return CipherType.Vigenere;
+        } else {
+            return CipherType.EMPTY;
+        }
+    }
+}
+
+enum CipherType {
+    Caesar, Transposition, Vigenere, EMPTY
+}
+
+enum CipherMethod {
+    Encrypt, Decrypt
+}
+
+class CipherFactory {
+    public static createCipher(type: CipherType, method: CipherMethod): ICipher {
+        if (type == CipherType.Caesar) {
+            return CipherFactory.createCeasarCipher(method);
+        } else if (type == CipherType.Transposition) {
+            return CipherFactory.createTranspositionCipher(method);
+        } else {
+            return CipherFactory.createVigenereCipher(method);
+        }
+    }
+
+    private static createCeasarCipher(method: CipherMethod): ICipher {
+        return method == CipherMethod.Encrypt ? new CaesarCipherEncoder : new CaesarCipherDecoder;
+    }
+
+    private static createTranspositionCipher(method: CipherMethod): ICipher {
+      return method == CipherMethod.Encrypt ? new CaesarCipherEncoder : new CaesarCipherDecoder;
+    }
+
+    private static createVigenereCipher(method: CipherMethod): ICipher {
+       return method == CipherMethod.Encrypt ? new CaesarCipherEncoder : new CaesarCipherDecoder;
+    }
+}
--- src/site/ts/index.ts
+++ src/site/ts/index.ts
@@ -1,16 +1,28 @@
-import { cipherForm } from './formValidation/controller/cipherFormController';
-require('../css/application.css');
+import { CipherController } from "./ciphers/controllers/CipherController";
+import { cipherForm } from "./formValidation/controller/cipherFormController";
+require("../css/application.css");
 
-const keyInputElement: HTMLInputElement = <HTMLInputElement>document.getElementById('key');
-const cipherType: HTMLInputElement = <HTMLInputElement>document.getElementById('selectCipher');
-const submitButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById('button');
+const keyInputElement = <HTMLInputElement>document.getElementById("key");
+const cipherType = <HTMLInputElement>document.getElementById("selectCipher");
+const submitButton = <HTMLButtonElement>document.getElementById("button");
+const cipherTextArea = <HTMLTextAreaElement>document.getElementsByClassName(
+  "cipherTextArea"
+)[0];
+
+const output = <HTMLParagraphElement>document.getElementById('cipherOutput');
 
 const cipherFormController = new cipherForm();
 cipherFormController.validateForm(cipherType, keyInputElement, submitButton);
 cipherFormController.ensureCorrectPlaceholder(cipherType, keyInputElement);
 
-
+const cipher = new CipherController();
+submitButton.addEventListener("click", () => {
+  if (submitButton.disabled == false) {
+    const cipherText: string = cipherTextArea.value;
+    output.innerHTML = cipher.encrypt(cipherText, keyInputElement.value, cipherType.value);
+  }
+});
 
 if (module.hot) {
-    module.hot.accept();
+  module.hot.accept();
 }
